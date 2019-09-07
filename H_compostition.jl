548e8438eda69af8d9f1e23b6a3a18013d91ace0
yongchuly <hsc930526@gmail.com>
2019-09-07 23:27:07 +0900

    Update H_compostition.jl

--- H_compostition.jl
+++ H_compostition.jl
@@ -35,3 +35,54 @@ beta = fill(1, 123)
 hessian_mat(beta, newz)
 
 # ifelse.(1 .- beta .< 0, 1., 0.)
+
+using Convex, SCS
+
+m = 4; n = 5
+
+A = randn(m, n); b = randn(m, 1)
+
+x = Variable(n)
+
+problem = minimize(sumsquares(A * x - b), [x >= 0])
+
+
+#####
+
+rho = 0.5
+
+n, p = size(newz)
+
+### Initial 
+
+tmp_gamma = randn(p*4)
+
+tmp_nu = randn(p*4)
+
+tmp_u = tmp_nu / rho
+
+tmp_beta = fill(0, p)
+
+Amat = fill(1, p)
+
+### for loop
+
+for i in 1:10000
+
+    d = tmp_u - tmp_nu
+
+    grad = gradient_vec(tmp_beta, newz, y)
+
+    hm = hessian_mat(tmp_beta, newz)
+
+    x = Variable(p)
+
+    problem = minimize(quadform(x, rho * (A'* A + hm)) + (tmp_beta' * hm - grad - rho*d*Amat)'*x, Amat' * x = 1)
+
+    solve!(problem, SCSSolver())
+
+    problem.status
+
+    x.value # optimal value
+
+end
